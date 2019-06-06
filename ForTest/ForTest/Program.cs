53be7a6e8027e20dce5c95b874303d31a2fcc528
Marcin Bala <51169435+marcepanb@users.noreply.github.com>
2019-06-07 01:29:04 +0200

    Update Program.cs

--- ForTest/ForTest/Program.cs
+++ ForTest/ForTest/Program.cs
@@ -1,20 +1,25 @@
-﻿using System;
+using System;
 
 namespace ForTest {
+
+	class SomeClass<T> where T : struct, IComparable, IComparable<T>, IConvertible, IEquatable<T>, IFormattable {
+		// T może być tylko numeryczne aka int, char, byte
+	}
+
+	interface INumeral<T> : IComparable, IComparable<T>, IConvertible, IEquatable<T>, IFormattable {
+		// pomocniczy interface dzięki któremu mogę skrócić pisanie
+	}
+
+	class SomeOtherClass<T> where T : struct, INumeral<T> {
+		// klasa z użytym skrótem
+	}
+
 	class Program {
 		static void Main(string[] args) {
 
-			SomeClass<int> someClass = new SomeClass<int>();
-			SomeOtherClass<int> someOtherClass = new SomeOtherClass<int>();
+			SomeClass<int> someClass = new SomeClass<int>();                // działa dobrze
+			SomeClass<string> someClass = new SomeClass<string>();			// zgodnie z potrzebą T musi być numeryczne 
+			SomeOtherClass<int> someOtherClass = new SomeOtherClass<int>();	// ale użycie skrótu nie działa 
 		}
 	}
-
-
-	interface INumeral<T> : IComparable, IComparable<T>, IConvertible, IEquatable<T>, IFormattable { }
-
-	class SomeClass<T> where T : struct, IComparable, IComparable<T>, IConvertible, IEquatable<T>, IFormattable { }
-
-	class SomeOtherClass<T> where T : struct, INumeral<T> { }
-
-
 }
