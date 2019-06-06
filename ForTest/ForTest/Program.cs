64ee7c80aaf5a877e758dbf4276482a8052a412f
Marcin Bala <51169435+marcepanb@users.noreply.github.com>
2019-06-07 01:08:54 +0200

    Add files via upload

---
+++ ForTest/ForTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ForTest {
+	class Program {
+		static void Main(string[] args) {
+
+			SomeClass<int> someClass = new SomeClass<int>();
+			SomeOtherClass<int> someOtherClass = new SomeOtherClass<int>();
+		}
+	}
+
+
+	interface INumeral<T> : IComparable, IComparable<T>, IConvertible, IEquatable<T>, IFormattable { }
+
+	class SomeClass<T> where T : struct, IComparable, IComparable<T>, IConvertible, IEquatable<T>, IFormattable { }
+
+	class SomeOtherClass<T> where T : struct, INumeral<T> { }
+
+
+}
